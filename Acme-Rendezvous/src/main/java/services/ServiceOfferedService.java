79af3155924c097194be14742774b4c959a3e653
joaquinperez94 <blogdeaula78@gmail.com>
2018-03-07 09:11:47 +0100

    Cambios en el service

--- Acme-Rendezvous/src/main/java/services/ServiceOfferedService.java
+++ Acme-Rendezvous/src/main/java/services/ServiceOfferedService.java
@@ -1,9 +1,16 @@
 
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import repositories.ServiceOfferedRepository;
+import domain.ServiceOffered;
 
 @Service
 @Transactional
@@ -22,4 +29,24 @@ public class ServiceOfferedService {
 	}
 
 	// Simple CRUD methods-----------------------------------------------------
+
+	//Create
+
+	//Save
+
+	//Delete
+
+	public Collection<ServiceOffered> findAll() {
+		Collection<ServiceOffered> result;
+		result = new ArrayList<ServiceOffered>(this.serviceOfferedRepository.findAll());
+		Assert.notNull(result);
+		return result;
+	}
+
+	public ServiceOffered findOne(final int serviceOfferedId) {
+		ServiceOffered result;
+		Assert.isTrue(serviceOfferedId != 0);
+		result = this.serviceOfferedRepository.findOne(serviceOfferedId);
+		return result;
+	}
 }
