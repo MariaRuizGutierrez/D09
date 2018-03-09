0bd6a01e7c9626a7210f90c843609ce923cbd508
espisepi <sepinaco@gmail.com>
2018-03-09 18:22:14 +0100

    feat: añadido mas test en RendezvouseServiceTest
    commit para ver donde falla el codigo en el siguiente commit que haga

--- Acme-Rendezvous/src/test/java/services/RendezvouseServiceTest.java
+++ Acme-Rendezvous/src/test/java/services/RendezvouseServiceTest.java
@@ -36,36 +36,54 @@ public class RendezvouseServiceTest extends AbstractTest {
 	public void driverCreateAndSave() {
 		final Collection<GPS> listGPS = this.createAllGPSForTesting();
 		final Iterator<GPS> iterator = listGPS.iterator();
+		final GPS gpsOk = iterator.next();
 		final Object testingData[][] = {
+			//			{
+			//				//Se crea un Rendezvouse correctamente
+			//				"user1", "name test", "description", "2019/03/03", "http://www.test.com", gpsOk, true, false, false, null
+			//			}, {
+			//				//Se crea un Rendezvouse correctamente con Gps con latitude null
+			//				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, null
+			//			}, {
+			//				//Se crea un Rendezvouse correctamente con Gps con longitude null
+			//				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, null
+			//			}, {
+			//				//Se crea un Rendezvouse incorrectamente con Gps con OutOfRangeLatitudeMax
+			//				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, javax.validation.ConstraintViolationException.class
+			//			}, {
+			//				//Se crea un Rendezvouse incorrectamente con Gps con OutOfRangeLatitudeMin
+			//				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, javax.validation.ConstraintViolationException.class
+			//			}, {
+			//				//Se crea un Rendezvouse incorrectamente con Gps con OutOfRangeLongitudeMax
+			//				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, javax.validation.ConstraintViolationException.class
+			//			}, {
+			//				//Se crea un Rendezvouse incorrectamente con Gps con OutOfRangeLongitudeMin
+			//				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, javax.validation.ConstraintViolationException.class
+			//			}, {
+			//				//Se crea un Rendezvouse incorrectamente con title en blank
+			//				"user5", "", "description", "2019/03/03", "http://www.test.com", gpsOk, true, false, false, javax.validation.ConstraintViolationException.class
+			//			}, 
 			{
-				//Se crea un Rendezvouse correctamente
-				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, null
+				//Se crea un Rendezvouse incorrectamente con description en blank
+				"user5", "name test", "", "2019/03/03", "http://www.test.com", gpsOk, true, false, false, javax.validation.ConstraintViolationException.class
 			}, {
-				//Se crea un Rendezvouse correctamente con Gps con latitude null
-				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, null
-			}, {
-				//Se crea un Rendezvouse correctamente con Gps con longitude null
-				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, null
-			}, {
-				//Se crea un Rendezvouse incorrectamente con Gps con OutOfRangeLatitudeMax
-				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, javax.validation.ConstraintViolationException.class
-			}, {
-				//Se crea un Rendezvouse incorrectamente con Gps con OutOfRangeLatitudeMin
-				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, javax.validation.ConstraintViolationException.class
-			}, {
-				//Se crea un Rendezvouse incorrectamente con Gps con OutOfRangeLongitudeMax
-				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, javax.validation.ConstraintViolationException.class
-			}, {
-				//Se crea un Rendezvouse incorrectamente con Gps con OutOfRangeLongitudeMin
-				"user1", "name test", "description", "2019/03/03", "http://www.test.com", iterator.next(), true, false, false, javax.validation.ConstraintViolationException.class
+				//Se crea un Rendezvouse incorrectamente con organisedMoment en null
+				//Salta un NullPointerException en vez de javax.validation porque salta el Assert.isTrue que comprueba que la fecha introducida este en futuro
+				"user1", "name test", "description", null, "http://www.test.com", gpsOk, true, false, false, NullPointerException.class
 			}
+		//			, {
+		//				//Se crea un Rendezvouse correctamente con picture en null
+		//				"user1", "name test", "description", "2019/03/03", null, gpsOk, true, false, false, null
+		//			}, {
+		//				//Se crea un Rendezvouse incorrectamente con picture con url malamente
+		//				"user1", "name test", "description", "2019/03/03", "estoNoEsUnaURL", gpsOk, true, false, false, javax.validation.ConstraintViolationException.class
+		//			}
 		};
 
 		for (int i = 0; i < testingData.length; i++)
 			this.templateCreateAndSave((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (GPS) testingData[i][5], (boolean) testingData[i][6],
 				(boolean) testingData[i][7], (boolean) testingData[i][8], (Class<?>) testingData[i][9]);
 	}
-
 	private void templateCreateAndSave(final String username, final String name, final String description, final String organisedMoment, final String picture, final GPS gps, final boolean draftMode, final boolean deleted, final boolean forAdult,
 		final Class<?> expected) {
 		Rendezvouse rendezvouse;
@@ -78,7 +96,10 @@ public class RendezvouseServiceTest extends AbstractTest {
 			rendezvouse = this.rendezvouseService.create();
 			rendezvouse.setName(name);
 			rendezvouse.setDescription(description);
-			organisedMomentDate = (new SimpleDateFormat("yyyy/MM/dd")).parse(organisedMoment);
+			if (organisedMoment != null)
+				organisedMomentDate = (new SimpleDateFormat("yyyy/MM/dd")).parse(organisedMoment);
+			else
+				organisedMomentDate = null;
 			rendezvouse.setOrganisedMoment(organisedMomentDate);
 			rendezvouse.setPicture(picture);
 			rendezvouse.setGps(gps);
@@ -93,6 +114,7 @@ public class RendezvouseServiceTest extends AbstractTest {
 
 		this.checkExceptions(expected, caught);
 
+		super.unauthenticate();
 	}
 
 	//Other Methods additionals---------------------------------------------------------------------------------------
