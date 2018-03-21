aa07772c1a7506cb2caa387fc4f184d4a01ebefe
joaquinperez94 <blogdeaula78@gmail.com>
2018-03-21 16:35:38 +0100

    display para espi

--- Acme-Rendezvous/src/main/java/controllers/administrator/RendezvousAdministratorController.java
+++ Acme-Rendezvous/src/main/java/controllers/administrator/RendezvousAdministratorController.java
@@ -8,11 +8,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.AnnouncementService;
+import services.QuestionService;
 import services.RendezvouseService;
 import controllers.AbstractController;
+import domain.Announcement;
+import domain.Question;
 import domain.Rendezvouse;
+import domain.ServiceOffered;
 
 @Controller
 @RequestMapping("/rendezvous/administrator")
@@ -23,11 +29,44 @@ public class RendezvousAdministratorController extends AbstractController {
 	@Autowired
 	private RendezvouseService	rendezvouseService;
 
+	@Autowired
+	private QuestionService		questionService;
+	@Autowired
+	private AnnouncementService	announcementService;
+
 
 	//constructor-------------------------------------------------------------------------
 	public RendezvousAdministratorController() {
 		super();
 	}
+
+	//Display-----------------------------------------------------------------
+	@RequestMapping(value = "/display", method = RequestMethod.GET)
+	public ModelAndView display(@RequestParam final int rendezvousId) {
+		ModelAndView result;
+		Rendezvouse ren = new Rendezvouse();
+		Collection<Announcement> announcements;
+		Collection<ServiceOffered> services;
+		Collection<Question> questions;
+		Collection<Rendezvouse> similarRendezvouses;
+
+		ren = this.rendezvouseService.findOne(rendezvousId);
+		questions = this.questionService.findAllQuestionsByRendezvous(rendezvousId);
+		services = this.rendezvouseService.findAllServicesByRendezvous(rendezvousId);
+		announcements = this.announcementService.findAnnouncementByRendezvousId(rendezvousId);
+		similarRendezvouses = ren.getSimilarRendezvouses();
+
+		result = new ModelAndView("rendezvous/display");
+		result.addObject("rendezvous", ren);
+		result.addObject("announcements", announcements);
+		result.addObject("services", services);
+		result.addObject("questions", questions);
+		result.addObject("similarRendezvouses", similarRendezvouses);
+		result.addObject("requestURI", "rendezvous/administrator/display.do");
+
+		return result;
+	}
+
 	//Listing-----------------------------------------------------------------
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
