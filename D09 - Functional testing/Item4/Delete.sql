72e7a97820cdd49dfc2992bd1e6852948246c96b
lauverrec <lauraverarec@gmail.com>
2018-03-23 16:50:23 +0100

    fix: nuevo despliegue

--- D09 - Functional testing/Item4/Delete.sql
+++ D09 - Functional testing/Item4/Delete.sql
@@ -1,8 +1,8 @@
 start transaction;
-use `Acme-Rendezvous`;
-revoke all privileges on `Acme-Rendezvous`.* from 'acme-user'@'%';
-revoke all privileges on `Acme-Rendezvous`.* from 'acme-manager'@'%';
+use `Acme-Rendezvous2.0`;
+revoke all privileges on `Acme-Rendezvous2,0`.* from 'acme-user'@'%';
+revoke all privileges on `Acme-Rendezvous2.0`.* from 'acme-manager'@'%';
 drop user 'acme-user'@'%';
 drop user 'acme-manager'@'%';
-drop database `Acme-Rendezvous`;
+drop database `Acme-Rendezvous2.0`;
 commit;
